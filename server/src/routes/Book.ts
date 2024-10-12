20c8f56724767a7646069c9b8c4a7881801b2846
Node User <node_user@gmail.com>
2024-10-12 15:42:24 +0400

    Bug fix for book routes.

--- server/src/routes/Book.ts
+++ server/src/routes/Book.ts
@@ -5,10 +5,10 @@ import { createBook, getAllBooks, editBook, deleteBook } from '../controllers/bo
 const router = express.Router();
 
 // Handle the POST request to create a book.
-router.post("/book/create-book", createBook);
+router.post("/create-book", createBook);
 // router.get("/get-book", getBook);
-router.get("/book/get-all-books", getAllBooks);
-router.put("/book/", editBook);
-router.delete("/book/", deleteBook);
+router.get("/get-all-books", getAllBooks);
+router.put("/", editBook);
+router.delete("/", deleteBook);
 
 export default router;
